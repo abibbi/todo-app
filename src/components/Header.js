113706e45aed0791bc54f5bc9a03428db4a81ce3
JGant <JustinGant1@macbook-pro.lan>
2024-08-16 19:34:37 -0400

    UpdatedUi

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css'; 
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>To-Do List</h1>
+    </header>
+  );
+}
+
+export default Header;
