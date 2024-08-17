3eb23a5309c49fda33d0da5b3594377c26ee0512
abibbi <tony@anthonys-mbp.attlocal.net>
2024-08-16 21:04:06 -0400

    css change

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='list-items'>
+          <p>too doo list</p>
+          
+          </div>
+          <div className='list-items'>
+            <button className='delete'>trash</button>
+          <p>too doo list</p>
+          
+          </div>
+          <div className='list-items'>
+          <p>too doo list</p>
+          
+          </div>
+          hi
       </header>
     </div>
   );
